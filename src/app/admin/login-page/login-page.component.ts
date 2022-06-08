2281889ab515f4d372d863b05fc957ef6f4aec4e
VitSmol <smolitskiy@mail.ru>
2022-06-08 16:32:08 +0300

    frst commit

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -20,8 +20,8 @@ export class LoginPageComponent implements OnInit {
 
   ngOnInit(): void {
       this.form = new FormGroup({
-      email: new FormControl('ok@gomeluzo.by', [Validators.required, Validators.email]),
-      password: new FormControl('ok310522', [Validators.required, Validators.minLength(6)]),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(6)]),
     })
   }
   submit() {
